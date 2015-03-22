a444a9524300a94d2760333c96d6172f5816f26f
Thomas Chan <t39chan@uwaterloo.ca>
2015-03-22 17:24:11 -0400

    Change doctor and patient back to using d_alias and p_alias

--- src/java/ece356_project/User.java
+++ src/java/ece356_project/User.java
@@ -1,14 +1,16 @@
 package ece356_project;
 
 public class User {
+    String user_alias;
     String first_name;
     String last_name;
     String email_address;
     String password_hash;
     String password_salt;
 	
-    public User (String first_name, String last_name, String email_address, String password_hash, String password_salt)
+    public User (String user_alias, String first_name, String last_name, String email_address, String password_hash, String password_salt)
     {
+        this.user_alias = user_alias;
 	this.first_name = first_name;
 	this.last_name = last_name;
 	this.email_address = email_address;
