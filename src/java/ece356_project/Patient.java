a88a3c2e9cb1a31654dd13e16303e111692712c9
Thomas Chan <t39chan@uwaterloo.ca>
2015-03-22 15:39:55 -0400

    Merge doctor and patient alias as user alias

--- src/java/ece356_project/Patient.java
+++ src/java/ece356_project/Patient.java
@@ -5,10 +5,10 @@ public class Patient extends User{
 	String home_address_province;
 	String home_address_city;
 	
-	public Patient(String user_alias, String first_name, String last_name, String email_address, String password_hash, String password_salt,String p_alias, String home_address_province, String home_address_city)
+	public Patient(String user_alias, String first_name, String last_name, String email_address, String password_hash, String password_salt, String home_address_province, String home_address_city)
 	{
-		super(user_alias, first_name, last_name, email_address, password_hash, password_salt);
-		this.p_alias = p_alias;
+		super(first_name, last_name, email_address, password_hash, password_salt);
+		this.p_alias = user_alias;
 		this.home_address_province = home_address_province;
 		this.home_address_city = home_address_city;
 	}
--- src/java/ece356_project/User.java
+++ src/java/ece356_project/User.java
@@ -1,20 +1,18 @@
 package ece356_project;
 
 public class User {
-    String user_alias;
-	String first_name;
-	String last_name;
-	String email_address;
-	String password_hash;
-	String password_salt;
+    String first_name;
+    String last_name;
+    String email_address;
+    String password_hash;
+    String password_salt;
 	
-	public User (String user_alias, String first_name, String last_name, String email_address, String password_hash, String password_salt)
-	{
-		this.user_alias = user_alias;
-		this.first_name = first_name;
-		this.last_name = last_name;
-		this.email_address = email_address;
-		this.password_hash = password_hash;
-		this.password_salt = password_salt;
-	}
+    public User (String first_name, String last_name, String email_address, String password_hash, String password_salt)
+    {
+	this.first_name = first_name;
+	this.last_name = last_name;
+	this.email_address = email_address;
+	this.password_hash = password_hash;
+	this.password_salt = password_salt;
+    }
 }
