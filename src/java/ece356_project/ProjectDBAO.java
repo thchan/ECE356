d1ffa05fb698f648ddda8c0a5892f5551be9eb07
Thomas Chan <t39chan@uwaterloo.ca>
2015-03-22 22:21:41 -0400

    Add doctor search and doctor view profile queries

--- src/java/ece356_project/ProjectDBAO.java
+++ src/java/ece356_project/ProjectDBAO.java
@@ -92,8 +92,178 @@ public class ProjectDBAO {
                              resultSet.getString("email_address"),
                              resultSet.getInt("count(distinct review_id)"),
                              resultSet.getDate("max(date)"));
+                     ret.add(e);
                 }
                  
+                return ret;
+            } finally {
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+                    
+                if (con != null) {
+                    con.close();
+                }
+            }
+        }
+        
+        
+        public static ArrayList<Doctor> searchDoctors(String first_name, String last_name, String address, String gender, int licence_year, String comments, int rating)
+            throws ClassNotFoundException, SQLException, NamingException {
+		
+            Connection con = null;
+            PreparedStatement pstmt = null;
+            ArrayList<Doctor> ret = null;
+            
+            try{
+                con = getConnection();
+                
+                /* Build SQL query */
+                String query = "SELECT user_alias,d_alias, first_name ,last_name, gender, AVG(rating) , COUNT(distinct review_id), (YEAR(curdate()) - license_year) AS number_of_years_licensed, ";
+                query += "(CASE  WHEN d_alias IN\n" +
+                         "(SELECT d_alias FROM\n" +
+                         "((SELECT d_alias\n" +
+                         "FROM Review\n" +
+                         "WHERE p_alias IN (\n" +
+                         "    (SELECT p_alias_a\n" +
+                         "    FROM Friend INNER JOIN PatientData\n" +
+                         "    ON (PatientData.p_alias = Friend.p_alias_a ) \n" +
+                         "    WHERE Friend.p_alias_b = \"?(current_user_alias)\" AND flag = TRUE)\n" +
+                         "))\n" +
+                         "UNION  \n" +
+                         "(SELECT d_alias\n" +
+                         "FROM Review\n" +
+                         "WHERE p_alias IN (\n" +
+                         "    (SELECT p_alias_b\n" +
+                         "    FROM Friend INNER JOIN PatientData\n" +
+                         "    ON (PatientData.p_alias = Friend.p_alias_b ) \n" +
+                         "    WHERE Friend.p_alias_a = \"?(current_user_alias)\" AND flag = TRUE)\n" +
+                         "))) AS temp)\n" +
+                         "         THEN TRUE ELSE FALSE END \n" +
+                         ") AS friend_reviewed";
+                query += " FROM DoctorData NATURAL JOIN Review";
+                query += " WHERE TRUE ";
+                
+                if (first_name.length() != 0){
+                    query += " AND first_name like %?%";
+                }
+                
+                if (last_name.length() != 0){
+                    query += " AND last_name like %?%";
+                }
+                
+                if (gender.length() != 0){
+                    query += " AND gender = ?";
+                }
+                
+                if (licence_year != -1){
+                    query += " AND (YEAR(curdate()) - license_year) > ?";
+                }
+                
+                if (comments.length() != 0){
+                    query += " AND LCASE(comments) like LCASE('%?%')";
+                }
+                
+                query += " GROUP BY (d_alias)";
+                
+                if (rating != -1){
+                    query += " HAVING (AVG(rating) > ?";
+                }
+                
+                pstmt = con.prepareStatement(query);
+                
+                int num = 0;
+                if (first_name.length() != 0){
+                    pstmt.setString(++num, first_name);
+                }
+                
+                if (last_name.length() != 0){
+                    pstmt.setString(++num, last_name);
+                }
+                
+                if (gender.length() != 0){
+                    pstmt.setString(++num, gender);
+                }
+                
+                if (licence_year != -1){
+                    pstmt.setInt(++num, licence_year);
+                }
+                
+                if (comments.length() != 0){
+                    pstmt.setString(++num, comments);
+                }
+                
+                query += " GROUP BY (d_alias)";
+                
+                if (rating != -1){
+                    pstmt.setInt(++num, rating);
+                }
+                
+                 ResultSet resultSet;
+                 resultSet = pstmt.executeQuery();
+                 
+                 ret = new ArrayList<Doctor>();
+                 while (resultSet.next()){
+                     Doctor e = new Doctor(
+                             resultSet.getString("user_alias"),
+                             resultSet.getString("DoctorData.d_alias"),
+                             resultSet.getString("first_name"),
+                             resultSet.getString("last_name"),
+                             resultSet.getString("gender"),
+                             resultSet.getString("email_address"),
+                             resultSet.getInt("AVG(rating)"),
+                             resultSet.getInt("COUNT(distinct review_id)"),
+                             resultSet.getInt("number_of_years_licensed"),
+                             resultSet.getBoolean("friend_reviewed"));
+                     ret.add(e);
+                }
+                 
+                return ret;
+            } finally {
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+                    
+                if (con != null) {
+                    con.close();
+                }
+            }
+                    }
+        
+        
+        public static Doctor getDoctorProfile(String d_alias)
+            throws ClassNotFoundException, SQLException, NamingException {
+		
+            Connection con = null;
+            PreparedStatement pstmt = null;
+            Doctor ret = null;
+            
+            try{
+                con = getConnection();
+                
+                /* Build SQL query */
+                String query = "SELECT user_alias, first_name ,last_name, email_address, gender, AVG(rating) , COUNT(distinct review_id), (YEAR(curdate()) - license_year) AS number_of_years_licensed ";
+                query += " FROM DoctorData NATURAL JOIN Review";
+                query += " WHERE d_alias = ? ";
+                
+                pstmt = con.prepareStatement(query);
+                
+                pstmt.setString(1, d_alias);
+                
+                 ResultSet resultSet;
+                 resultSet = pstmt.executeQuery();
+                 
+                ret =  new Doctor(
+                             resultSet.getString("user_alias"),
+                             d_alias,
+                             resultSet.getString("first_name"),
+                             resultSet.getString("last_name"),
+                             resultSet.getString("gender"),
+                             resultSet.getString("email_address"),
+                             resultSet.getInt("AVG(rating)"),
+                             resultSet.getInt("COUNT(distinct review_id)"),
+                             resultSet.getInt("number_of_years_licensed"),
+                             false);
                 return ret;
             } finally {
                 if (pstmt != null) {
