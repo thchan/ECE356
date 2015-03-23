58bac2e7f66af99984fa8db5ccb3f96291bcd4df
Thomas Chan <t39chan@uwaterloo.ca>
2015-03-22 20:48:17 -0400

    Changes to Doctor and Patient classes

--- src/java/ece356_project/Doctor.java
+++ src/java/ece356_project/Doctor.java
@@ -1,16 +1,29 @@
 
 package ece356_project;
 
-public class Doctor extends User{
+import java.util.Calendar;
+
+public class Doctor{
+    public String user_alias;
     public String d_alias;
+    public String first_name;
+    public String last_name;
+    public String email_address;
     public String gender;
     public int license_year;
-	
-    public Doctor(String user_alias, String first_name, String last_name, String email_address, String password_hash, String password_salt, String d_alias, String gender, int license_year)
+    public int average_rating;
+    public int number_of_reviews;
+    
+    public Doctor(String user_alias, String d_alias, String first_name, String last_name, String email_address, String gender, int license_year, int average_rating, int number_of_reviews)
     {
-	super(user_alias, first_name, last_name, email_address, password_hash, password_salt);
+	this.user_alias = user_alias;
         this.d_alias = d_alias;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.email_address = email_address;
 	this.gender = gender;
-	this.license_year = license_year;
+	this.license_year = Calendar.getInstance().get(Calendar.YEAR) - license_year;
+        this.average_rating = average_rating;
+        this.number_of_reviews = number_of_reviews;
     }
 }
--- src/java/ece356_project/Patient.java
+++ src/java/ece356_project/Patient.java
@@ -1,15 +1,23 @@
 package ece356_project;
 
-public class Patient extends User{
+import java.sql.Date;
+
+public class Patient{
+    public String user_alias;
     public String p_alias;
     public String home_address_province;
     public String home_address_city;
+    public String email;
+    public int number_of_reviews;
+    public Date last_review_date;
 	
-	public Patient(String user_alias, String first_name, String last_name, String email_address, String password_hash, String password_salt, String p_alias, String home_address_province, String home_address_city)
-	{
-            super(user_alias ,first_name, last_name, email_address, password_hash, password_salt);
+	public Patient(String user_alias, String p_alias, String home_address_province, String home_address_city, String email, int number_of_reviews, Date last_review_date){
+            this.user_alias = user_alias;
             this.p_alias = p_alias;
             this.home_address_province = home_address_province;
             this.home_address_city = home_address_city;
+            this.email = email;
+            this.number_of_reviews = number_of_reviews;
+            this.last_review_date = last_review_date;
 	}
 }
